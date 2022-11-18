7fca60ffd5d6790457a67c8b5988154705a9412f
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2022-11-18 10:15:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-# from flask import Flask,Response,jsonify, render_template ,logging,request
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# #run server
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=80)
-from flask import *
-import os
-import socket
-
+from flask import Flask,Response,jsonify, render_template ,logging,request
 app = Flask(__name__)
 
-# app.config["Debug"] = True
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('index.html')
 
+#run server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+# from flask import *
+# import os
+# import socket
+
+# app = Flask(__name__)
+
+# # app.config["Debug"] = True
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return render_template('index.html')
+
     
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 
-PORT = int(os.environ.get("PORT", 80))
-if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
+# PORT = int(os.environ.get("PORT", 80))
+# if __name__ == '__main__':
+#     app.run(threaded=True,host='0.0.0.0',port=PORT)
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', port=80)
