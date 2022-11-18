9d928fcc064a2d0b92ec03ec2bdab5ad931702ed
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2022-11-18 09:56:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask,Response,jsonify, render_template ,logging,request
+# from flask import Flask,Response,jsonify, render_template ,logging,request
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# #run server
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)
+from flask import *
+import os
+import socket
+
 app = Flask(__name__)
 
+# app.config["Debug"] = True
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
 
-#run server
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run()
+
+
+PORT = int(os.environ.get("PORT", 9090))
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',port=PORT)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=9090)
