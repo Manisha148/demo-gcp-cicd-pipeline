696002f844953b7f83188b446928ffcc0ff53da0
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2022-11-18 10:08:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
     app.run()
 
 
-PORT = int(os.environ.get("PORT", 9090))
+PORT = int(os.environ.get("PORT", 80))
 if __name__ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=PORT)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=9090)
+  app.run(host='0.0.0.0', port=80)
